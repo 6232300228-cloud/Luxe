3c4c2e2e8993725ea687da32b13660f3af9ad7e9
Valeria <6232300228@univermilenium.edu.mx>
2026-04-09 17:33:24 -0600

    el sacapuntas ya cuesta 5 pesos

--- data/data.js
+++ data/data.js
@@ -47,7 +47,7 @@ const products = [
   { id: 42, name: "Brush Cleanser", price: 390, category: "accesorios", brand: "mac", img: "img/Brush cleanser.png", desc: "Limpiador de brochas de secado rapido." },
   { id: 43, name: "Prep + Prime Fix+", price: 680, category: "accesorios", brand: "mac", img: "img/prep+prime fix.png", desc: "Bruma de agua que fija el maquillaje y refresca." },
   { id: 44, name: "Matte Setting Spray", price: 235, category: "accesorios", brand: "nyx", img: "img/Matte setting.png", desc: "Fijador de maquillaje para evitar el brillo." },
-  { id: 45, name: "Sacapuntas de Cosméticos", price: 10, category: "accesorios", brand: "bissu", img: "img/Sacapuntas.png", desc: "Mantén tus lapices con punta perfecta." },
+  { id: 45, name: "Sacapuntas de Cosméticos", price: 5, category: "accesorios", brand: "bissu", img: "img/Sacapuntas.png", desc: "Mantén tus lapices con punta perfecta." },
   { id: 46, name: "Pegamento Lash It Loud", price: 190, category: "accesorios", brand: "nyx", img: "img/Pegamento.png", desc: "Adhesivo para pestañas de alta fijacion." },
   { id: 47, name: "Pestañas Postizas 3D", price: 85, category: "ojos", brand: "mac", img: "img/pestañas.png", desc: "Anade volumen y drama a tus ojos." },
   { id: 48, name: "Paleta Pro Contour", price: 320, category: "rubor", brand: "nyx", img: "img/contour.png", desc: "Kit para definir y resaltar facciones." },
